88cbdd8f38188a83ac50f68bafb4ebcbc7aecb92
username <Jzazzi56@gmail.com>
2024-07-10 11:19:20 -0700

    added a feature where when a button is clicked it toggles a different color to show the feature has been enabled

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -9,6 +9,7 @@ export default function Accordian() {
   const [selected, setSelected] = useState(null); //setting the state to null because nothing is selected initially
   const [enableMultiSelection, setEnableMultiSelection] = useState(false); //setting the state to false because multi selection is not enabled initially
   const [selectMultiple, setSelectMultiple] = useState([]); //setting the state to empty array because nothing is selected initially
+  const [color, setColor] = useState("red");
 
   const handleSingleSeletion = (getCurrentId) => {
     // console.log(getCurrentId);
@@ -28,14 +29,21 @@ export default function Accordian() {
     setSelectMultiple(cpySelectMuliple);
   };
   console.log(selected, selectMultiple);
+
+  const handleButtonClick = () => {
+    setEnableMultiSelection(!enableMultiSelection);
+    setColor(color === "red" ? "green" : "red");
+  };
+
   return (
     <>
       <div className="container">
         <Nav />
         <h1>Accordian</h1>
         <button
-          onClick={() => setEnableMultiSelection(!enableMultiSelection)}
+          onClick={handleButtonClick}
           className="multi-seletion-btn"
+          style={{ backgroundColor: color }}
         >
           Enable Multi Seletction
         </button>
